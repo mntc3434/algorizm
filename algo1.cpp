57a9b34cf6da925185934de9e37b4e8a6c1290da
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2024-12-27 02:38:42 +0300

    Update algo1.cpp

--- algo1.cpp
+++ algo1.cpp
@@ -43,3 +43,30 @@ public:
         }
         temp->next = newNode;
     }
+    void deleteNode(int value) {
+        if (head == nullptr) {
+            cout << "List is empty, cannot delete " << value << endl;
+            return;
+        }
+
+        if (head->data == value) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != nullptr && temp->next->data != value) {
+            temp = temp->next;
+        }
+
+        if (temp->next == nullptr) {
+            cout << "Value " << value << " not found in the list." << endl;
+            return;
+        }
+
+        Node* nodeToDelete = temp->next;
+        temp->next = temp->next->next;
+        delete nodeToDelete;
+    }
